82dda882b9fb33a73d931ea2994cd12a6f70cf62
YingFeng256 <yf2627181812@gmail.com>
2024-12-08 15:53:17 +0800

    feat: 启用部分组件

--- src/drawer.tsx
+++ src/drawer.tsx
@@ -1,13 +1,44 @@
-import React, {FunctionComponent} from 'react';
-import {RsdButton} from "oflow-interface"
+import React, { FunctionComponent } from 'react';
+import { RsdButton, RsdSelectableButton, RsdCheckbox, RsdColor, RsdNone, RsdTextarea, RsdTitle, Anchor } from "oflow-interface"
 
 export default function Drawer() {
   
   return <div>
-    <h1>PROJECT DRAWER</h1>
     <RsdButton
       text={'button'}
       onClick={()=>{}}
+    />
+    <RsdSelectableButton
+      value={true}
+      onChange={()=>{}}
+      label='RsdSelectableButton'
+    />
+    {/*<RsdCheckbox*/}
+    {/*  value={true}*/}
+    {/*  onChange={()=>{}}*/}
+    {/*  label='RsdCheckbox'*/}
+    {/*/>*/}
+    {/*<RsdColor*/}
+    {/*  value={'#ffffff'}*/}
+    {/*  onChange={()=>{}}*/}
+    {/*  label={'RsdColor'}*/}
+    {/*/>*/}
+    <RsdNone
+      label={'RsdNone'}
+    />
+    <RsdTextarea
+      value={''}
+      onChange={()=>{}}
+    />
+    {/*<RsdTitle*/}
+    {/*  title={'RsdTitle'}*/}
+    {/*/>*/}
+    <Anchor
+      list = {['All', 'Texture', 'Model', 'Image', 'Audio', 'Video', 'Materials', 'Others']}
+      value={1}
+      onSelectChange={(value: number) => {
+        console.log(value)
+      }}
     />
   </div>
 }
