3e0e6350a79b3827ccabdcf03c8ec044081ec5a3
YingFeng256 <yf2627181812@gmail.com>
2024-12-20 15:58:55 +0800

    fix: 修复Babylon无效

--- src/drawer/drawer.tsx
+++ src/drawer/drawer.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import {Babylon, RsdInput, useEffect, useState} from "../api";
 import styles from './drawer.module.sass'
 
-const {Vector3} = Babylon
-
 export default function Drawer() {
 
   const [title, setTitle] = useState('')
 
   useEffect(() => {
-    console.log(new Vector3(10, 20, 10))
+    console.log(new Babylon.Vector3(10, 20, 10))
   })
 
   return <div className={styles.drawer}>
