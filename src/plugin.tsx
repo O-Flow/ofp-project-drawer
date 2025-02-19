84ae95bf58231b4ea5a3f72a58d7c9ea4c9aacd3
HRan2004 <2125764918@qq.com>
2025-02-20 01:01:43 +0800

    feat: 补充data数据

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -15,6 +15,8 @@ export default class Plugin implements DrawerPlugin {
   type: PluginType = PluginType.DRAWER
   theme: ThemeColor = ThemeColor.BLUE
 
+  data = {}
+
   async onLoad() {
     loadApi()
   }
